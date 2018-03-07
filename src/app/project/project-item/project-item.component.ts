70ba7e41350ea2c07f6f05d817ff7222c94e2322
haohailiang <646263409@qq.com>
2018-03-07 15:20:28 +0800

    完成hover的效果

--- src/app/project/project-item/project-item.component.ts
+++ src/app/project/project-item/project-item.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding, HostListener } from '@angular/core';
+import { cardAnim } from '../../anims/card.anim';
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
-  styleUrls: ['./project-item.component.scss']
+  styleUrls: ['./project-item.component.scss'],
+  animations: [
+    cardAnim
+  ]
 })
 export class ProjectItemComponent implements OnInit {
   @Input() item;
   @Output() onInvite = new EventEmitter<void>();
   @Output() onEdit = new EventEmitter<void>();
   @Output() onDel = new EventEmitter<void>();
+  @HostBinding('@card') cardState = 'out';
 
   constructor() { }
 
@@ -26,6 +31,16 @@ export class ProjectItemComponent implements OnInit {
 
   onDelClick() {
     this.onDel.emit();
+  }
+
+  @HostListener('mouseenter', ['$event.target'])
+  onMouseEnter(target) {
+    this.cardState = 'hover';
+  }
+
+  @HostListener('mouseleave', ['$event.target'])
+  onMouseLeave(target) {
+    this.cardState = 'out';
   }
 
 }
