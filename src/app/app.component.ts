4f3380e2df29a696a1b6b2a32c606da389480b09
haohailiang <646263409@qq.com>
2018-03-07 14:54:38 +0800

    测试动画沙盒取消

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { OverlayContainer } from '@angular/material';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('square', [
-      state('green', style({'background-color': 'green', 'height': '100px', 'transform': 'translateX(0)'})),
-      state('red', style({'background-color': 'red', 'height': '50px', 'transform': 'translateX(100%)'})),
-      transition('green => red', animate('5s')),
-      transition('red => green', animate('5s', keyframes([
-        style({'background-color': 'red'}),
-        style({'background-color': 'green'}),
-        style({'background-color': 'blue'})
-      ])))
-    ])
-  ]
 })
 export class AppComponent {
-  squareState: string;
   darkTheme = false;
 
   constructor(
@@ -34,8 +20,5 @@ export class AppComponent {
     this.darkTheme = darkTheme;
     this.oc.themeClass = darkTheme? 'myapp-dark-theme': null;
   }
-
-  onClick() {
-    this.squareState = this.squareState === 'green'? 'red': 'green';
-  }
+  
 }
