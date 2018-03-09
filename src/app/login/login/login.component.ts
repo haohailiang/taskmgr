e7b6b5ca494bcf97accc5f4ba41b60fa1ce9c603
haohailiang <646263409@qq.com>
2018-03-09 16:41:28 +0800

    响应式表单的复杂写法

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('wang@163.com', Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit({value, valid}, ev: Event) {
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(JSON.stringify(valid));
   }
 
 }
